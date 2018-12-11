9d367f20083028d66f97991b457f94c85daac034
d-g10d3k <40243886+d-g10d3k@users.noreply.github.com>
2018-12-11 17:16:28 +0100

    added a function that lists all of the key pairs

---
+++ keyTable.c
@@ -0,0 +1,41 @@
+#include "key_mingle.h"
+#include <stdio.h>
+
+char keyCodes[256][2] = {0}; //array of keys to swap
+
+//this function adds a pair of keys to the array
+int addKeys (char key1, char key2){
+    for (int i = 0; i < sizeof(keyCodes)/2; i++){
+        if (keyCodes[i][0] == 0){
+            keyCodes[i][0] = key1;
+            keyCodes[i][1] = key2;
+            return 0;
+        }
+    }
+    return 1;//there is no free space in the array
+}
+
+//displays all the key pairs from the keyCodes array
+int displayKeyPairs(){
+    for (int i = 0; i < sizeof(keyCodes)/2; i++){
+        if (keyCodes[i][0] != 0){
+             puts("------");
+             printf("%c (%d)", keyCodes[i][0], keyCodes[i][0]);
+             putc('\n', stdout);
+             printf("%c (%d)", keyCodes[i][1], keyCodes[i][1]);
+             putc('\n', stdout);
+        }
+    }
+    puts("-----");
+    return 0;
+}
+
+//this function removes a pair of keys from the array
+int removeKeys(int p){
+    if (p < sizeof(keyCodes)){
+        keyCodes[p][0] = 0;
+        keyCodes[p][1] = 0;
+        return 0;
+    }
+    return 1;//int pair is greater than the length of the array
+}
