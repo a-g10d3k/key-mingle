d84cf66e795847a2813eec70950412ba2b417274
d-g10d3k <40243886+d-g10d3k@users.noreply.github.com>
2018-12-11 17:15:29 +0100

    commented the part that changes all inputs to b
    also added a KBDLLHOOKSTRUCT that stores the key event information

--- KeyboardHook.c
+++ KeyboardHook.c
@@ -1,10 +1,13 @@
 #define _WIN32_WINNT 0x0500
 #include <windows.h>
 #include "key_mingle.h"
+#include <stdio.h>
+#include "stdioex.h"
 //This function is called every time a keyboard event is registered
 LRESULT __stdcall KeyboardHookCallback(int nCode, WPARAM wParam, LPARAM lParam){
     if (nCode >= 0){
 
+    KBDLLHOOKSTRUCT kbdStruct;
     INPUT inputs[1]; //create an array of input structs (actually a single input struct) that will later be fed to the SendInput function
     //set values for the input struct
     inputs[0].type           = INPUT_KEYBOARD;
@@ -13,10 +16,11 @@ LRESULT __stdcall KeyboardHookCallback(int nCode, WPARAM wParam, LPARAM lParam){
     inputs[0].ki.time        = 0;
     inputs[0].ki.dwExtraInfo = 0;
 
-            if(wParam == WM_KEYDOWN || wParam == WM_KEYUP){//check if the keyboad event is a keydown event
-                    inputs[0].ki.wVk = 0x42;//replace the keycode in the input struct with 'b'
-                    SendInput(1, inputs, sizeof(INPUT));//send input from the input struct
-                    return 1;//here we return one in order to prevent the keyboard event from being processed by any other process
+            if(wParam == WM_KEYDOWN || wParam == WM_KEYUP){//check if the keyboad event is a keydown or keyup event
+                    kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+                    //inputs[0].ki.wVk = 0x42;//replace the keycode in the input struct with 'b'
+                    //SendInput(1, inputs, sizeof(INPUT));//send input from the input struct
+                    //return 1;//here we return one in order to prevent the keyboard event from being processed by any other process
 
             }
     }
